5138b0614d0f2c8c86b894bac21b585293e4bfbb
Admin <admin@gmail.com>
2025-11-26 09:51:04 +0700

    backend ril

--- backend_bulbul_reservasi/app/Models/User.php
+++ backend_bulbul_reservasi/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -9,9 +10,10 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
+    use HasFactory;
 
     protected $fillable = [
-        'name',
+        'nama',
         'email',
         'password',
     ];
